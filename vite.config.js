fdd8924d0521973813e3783855c54a74593998b8
vedansh tiwari <vt2855028@gmail.com>
2025-05-01 22:38:33 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'   
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
-  base: "/wappi/wa-chat-generator/",
+  plugins: [react(), tailwindcss()],
+  base: "/",  // ✅ Root base for Vercel
 })
-
